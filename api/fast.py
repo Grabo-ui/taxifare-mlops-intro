cf08486cdc0360f6c9848026ce85ee60703b07c9
Christos Efthymiou <christosromanos.efthimiou204@gmail.com>
2026-09-03 14:42:51 +0300

    feat: add API skeleton with root and predict endpoints

---
+++ api/fast.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"greeting": "Hello"}
+
+@app.get("/predict")
+def predict(
+    pickup_datetime: str,
+    pickup_longitude: float,
+    pickup_latitude: float,
+    dropoff_longitude: float,
+    dropoff_latitude: float,
+    passenger_count: int
+):
+    return {'fare': float(passenger_count)}
